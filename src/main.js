89cf2f70a20f852402272aeb2eb0f3e959c15587
Christian Whitehouse <christian.whitehouse@gmail.com>
2020-05-18 22:15:53 -0400

    clean up example components

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import Vivere from './vivere/vivere.js';
 
-import Accordion from './examples/components/accordion.js';
+import Accordion from './examples/components/accordion/accordion.js';
 import AccordionSection from './examples/components/accordion/section.js';
-import Example from './examples/components/example.js';
-import InlineForm from './examples/components/inline-form.js';
 import ToDoItem from './examples/components/to-do/item.js';
 import ToDoList from './examples/components/to-do/list.js';
 import ToDoNewItem from './examples/components/to-do/new-item.js';
 
 Vivere.register('Accordion', Accordion);
 Vivere.register('AccordionSection', AccordionSection);
-Vivere.register('Example', Example);
-Vivere.register('InlineForm', InlineForm);
 Vivere.register('ToDoItem', ToDoItem);
 Vivere.register('ToDoList', ToDoList);
 Vivere.register('ToDoNewItem', ToDoNewItem);
