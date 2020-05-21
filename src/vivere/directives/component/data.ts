a760f0bea9f0d3b7f2d060e4b669458574cd2f22
Christian Whitehouse <christian.whitehouse@gmail.com>
2020-05-21 16:44:31 -0400

    throw error if our definition doesn't match with v-data

--- src/vivere/directives/component/data.ts
+++ src/vivere/directives/component/data.ts
@@ -1,5 +1,6 @@
 import Directive from '../directive';
 import Utility from '../../lib/utility';
+import VivereError from '../../error';
 
 export default class DataDirective extends Directive {
   static id = 'v-data';
@@ -9,9 +10,15 @@ export default class DataDirective extends Directive {
 
   parse(): void {
     let expression: unknown;
-    try { expression = JSON.parse(this.expression); } catch (err) { expression = this.expression; }
+    try { expression = JSON.parse(this.expression); }
+    catch (err) { expression = this.expression; }
 
+    const { $definition } = this.component;
     const camelKey = Utility.camelCase(this.key);
+    const keyDefined = camelKey in $definition.data?.();
+    if (!keyDefined)
+      throw new VivereError(`Component definitions must define any data passed with a v-data directive (${camelKey})`);
+
     this.component.$set(camelKey, expression);
   }
 }
