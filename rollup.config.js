7117c431fb5e1c533554510c1b70bdbdceafa767
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-10-05 20:58:50 -0400

    improve type output

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
 
 import { version } from "./package.json"
 const year = new Date().getFullYear()
@@ -30,6 +31,18 @@ export default [
     watch: {
       include: "src/**"
     }
+  },
+  {
+    input: "dist/types/src/vivere.d.ts",
+    output: [
+      {
+        file: "dist/vivere.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [
+      dts(),
+    ],
   },
   {
     input: "examples/main.ts",
