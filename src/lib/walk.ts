ff04daf377d3ba04f55bb749ad44405c4009f995
Christian Whitehouse <christian.whitehouse@gmail.com>
2022-02-05 15:42:16 -0500

    change our walk function to check every attribute then directives
    - generally a massive speedup
    - most elements have few to no attributes
    - also try to quickly skip elements with just a `class` attribute

--- src/lib/walk.ts
+++ src/lib/walk.ts
@@ -53,12 +53,12 @@ const directives: (typeof Directive)[] = [
 
 const Walk = {
   tree(element: Element, component?: VivereComponent): void {
-    const start = Date.now();
+    const start = performance.now();
 
     // Walk the tree to initialize components
     Walk.element(element, component);
 
-    const time = Date.now() - start;
+    const time = performance.now() - start;
     const method = time >= 100 ? 'warn' : 'log';
     // eslint-disable-next-line no-console
     console[method](`Vivere | Tree parsed: ${time}ms`);
@@ -71,21 +71,44 @@ const Walk = {
     // v-static stops tree walking for improved performance
     if (attributes['v-static'] != null) return;
 
-    // Loop through directives (so we can control parse order)
-    directives.forEach((Dir) => {
-      Object.values(element.attributes).forEach((attr) => {
-        const { name } = attr;
-        const { value } = attr;
+    // Track which directives we've found so we can parse them in order
+    const parsedDirectives: { Dir: typeof Directive, name: string, value: string }[] = [];
 
-        if (name.startsWith(Dir.id)) {
-          // Initialize and parse the directive
-          const directive = new Dir(element, name, value, $component);
+    // Looping through element attributes is way faster then looping
+    // through every directive for every element
+    Object.values(element.attributes).forEach((attr) => {
+      const { name, value } = attr;
+      // 'class' is our most common attribute, so we'll add a special
+      // case exception for noticing class
+      if (name === 'class') return;
 
-          // Re-assign component (for v-component directives)
-          $component = directive.component;
-        }
+      // Check for every directive we have registered
+      directives.forEach((Dir) => {
+        if (name.startsWith(Dir.id))
+          parsedDirectives.push({ Dir, name, value });
       });
     });
+
+    if (parsedDirectives.length) {
+      // Parse the directives in the proper order
+      parsedDirectives.sort((a, b) => {
+        const aIdx = directives.indexOf(a.Dir);
+        const bIdx = directives.indexOf(b.Dir);
+
+        if (aIdx > bIdx) return 1;
+        if (aIdx === bIdx) return 0;
+        return -1;
+      });
+
+      // Initialize every directive
+      parsedDirectives.forEach(({ Dir, name, value }) => {
+        // Initialize and parse the directive
+        const directive = new Dir(element, name, value, $component);
+
+        // Re-assign component (for v-component directives)
+        $component = directive.component;
+      });
+    }
 
     if (!(element instanceof HTMLElement) || element.dataset[Directive.DATA_SUSPEND_PARSING] !== 'true')
       Walk.children(element, $component);
