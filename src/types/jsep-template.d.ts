6879c7847c9d3e1a9c35511d14cffa13d838c4e8
Christian Whitehouse <christian.whitehouse@gmail.com>
2024-04-26 11:11:43 -0400

    fix type issue

---
+++ src/types/jsep-template.d.ts
@@ -0,0 +1,28 @@
+import jsep, { Expression, IPlugin } from 'jsep';
+
+export const name: string;
+export function init(this: typeof jsep): void;
+
+export interface TaggedTemplateExpression extends Expression {
+  type: 'TaggedTemplateExpression';
+  readonly tag: Expression;
+  readonly quasi: TemplateLiteral;
+}
+
+export interface TemplateElement extends Expression {
+  type: 'TemplateElement';
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  value: { cooked: string; raw: string };
+  tail: boolean;
+}
+
+export interface TemplateLiteral extends Expression {
+  type: 'TemplateLiteral';
+  quasis: TemplateElement[];
+  expressions: Expression[];
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
+declare const _export: IPlugin;
+export default _export;
