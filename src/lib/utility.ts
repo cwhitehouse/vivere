e23d116d6229963034661121c4aec2f27e72d147
Christian Whitehouse <christian.whitehouse@gmail.com>
2024-04-29 10:57:50 -0400

    handle unexpected strings for kebab case utility

--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -14,8 +14,8 @@ export default {
   kebabCase(name: string): string {
     return name
       .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-      .map((x) => x.toLowerCase())
-      .join('-');
+      ?.map((x) => x.toLowerCase())
+      ?.join('-') || name;
   },
 
   orderBy<T>(array: T[], keys: string[], directions: ('asc' | 'desc')[]): T[] {
